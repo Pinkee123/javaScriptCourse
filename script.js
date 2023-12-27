f48dc230b31a664404b09038829c6a9c30f46497
pinkee123 <hrithiksrivas33@gmail.com>
2023-12-27 10:15:43 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let FavFood="Pasta";
+let FavPlace="Banglore";
+let FavSubject="Database manaement system";
+let FavLanguage="Python";
+console.log(`My fav frood is ${FavFood} and my fav place is ${FavPlace}`)
